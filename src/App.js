4e89c050a7a20638f31ea9a507dd23ec05136f0d
AleksaP00 <popovicaleksa12@gmail.com>
2023-02-10 21:25:54 +0100

    Cetvrti commit- Kreirana logika za MovieCard i app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ import Contact from "./pages/Contact";
 import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 
+const API_URL = "http://www.omdbapi.com?apikey=bc3d1bdf";
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spider-man");
+  }, []);
+
   return (
     <BrowserRouter>
       <NavBar></NavBar>
